1695864bd81241636fb10567ab1760268f0f9d30
Yulia Glagola <julichis@mail.ru>
2024-01-11 22:15:25 +0400

    added book card component with basic styling

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
-const BookCard = ({ imageUrl = null, title = "", category = "", authors = "" }) => {
+const BookCard = ({ imageUrl, title, category, authors }) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{authors}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <img src={imageUrl} alt={title}/>
-      </CardContent>
-      <CardFooter>
-        <p>{category}</p>
-      </CardFooter>
-    </Card>
+    <div className="rounded-md bg-neutral-100 p-6 h-full">
+      <div className="h-2/3">
+        <img
+          src={imageUrl}
+          alt={title}
+          className="h-full p-2 mx-auto drop-shadow-xl"
+        />
+      </div>
+      <div className="text-start mt-3">
+        <div className={`text-sm text-gray-400 underline ${!category ? 'invisible' : ''}`}>
+          {category?.[0] ?? "no category"}
+        </div>
+        <div className="text-md font-bold leading-none line-clamp-2 mt-3">
+          {title}
+        </div>
+        <div className="text-sm leading-none line-clamp-2 mt-1 text-gray-400">
+          {authors}
+        </div>
+      </div>
+    </div>
   );
 };
 
