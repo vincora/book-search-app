673ab14fc5e292f4bf17739278908dd55d680d02
Yulia Glagola <julichis@mail.ru>
2024-01-13 19:13:22 +0400

    used flexbox instead of grid for books list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
 import "./App.css";
-import { Input } from "./components/ui/input";
+import { useState } from "react";
 import { useApiBooks } from "./api/useApiBooks";
 import BookCard from "./components/BookCard";
-import { useState } from "react";
+import Header from "./components/Header/Header";
 
 function App() {
   const booksQuery = useApiBooks();
   const [query, setQuery] = useState("");
-
   return (
-    <>
-      <div className="space-y-4">
-        <h1 className="text-2xl">Search for books</h1>
-        <Input placeholder=""></Input>
-      </div>
-      books
-      <ul>
+    <div className="space-y-4">
+      <Header />
+      <ul className="flex flex-wrap gap-5 justify-center">
         {booksQuery?.data?.items.map((item) => {
           return (
-            <li key={item.id}>
+            <li key={item.id} >
               <BookCard
                 imageUrl={item?.volumeInfo?.imageLinks?.smallThumbnail}
                 title={item.volumeInfo.title}
                 authors={item.volumeInfo.authors}
-                category={item.volumeInfo.categories[0]}
+                category={item.volumeInfo.categories}
               />
             </li>
           );
         })}
       </ul>
-    </>
+    </div>
   );
 }
 
