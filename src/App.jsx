5c94954938ffdaaed46bae65c84dee25936b4e2a
Yulia Glagola <julichis@mail.ru>
2024-01-14 20:59:18 +0400

    implemented form via react-hook-form and shadcn in header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useApiBooks } from "./api/useApiBooks";
 import BookCard from "./components/BookCard";
 import Header from "./components/Header/Header";
+import { fetchBooks } from "./api/fetchBooks";
 
 function App() {
   const booksQuery = useApiBooks();
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState({
+    input: '',
+    category: 'all',
+    sorting: 'relevance'
+  });
+
+  const handleSearch = (data) => {
+    console.log(data)
+    setQuery({
+      input: data.query,
+      category: data.category,
+      sorting: data.sorting
+    })
+  }
+
+
+  useEffect(() => {
+    fetchBooks(query)
+  }, [query])
+
   return (
     <div className="space-y-4">
-      <Header />
+      <Header onSearch={handleSearch}/>
       <ul className="flex flex-wrap gap-5 justify-center">
         {booksQuery?.data?.items.map((item) => {
           return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,48 +8,117 @@ import {
   SelectContent,
   SelectItem,
 } from "../ui/select";
-import imgUrl from './img/literature-composition.jpg?url';
+import imgUrl from "./img/literature-composition.jpg?url";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem } from "../ui/form";
 
+const Header = ({onSearch}) => {
+  const formSchema = z.object({
+    query: z.string(),
+    category: z.string(),
+    sort: z.string(),
+  });
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      query: "",
+      category: "all",
+      sort: "relevance",
+    },
+  });
 
-const Header = () => {
+  const handleSumbit = (formData) => {
+    console.log("onSubmit", { formData });
+    onSearch(formData)
+  };
   return (
-    <div className="space-y-5 p-7 bg-cover bg-center" style={{ backgroundImage: `url(${imgUrl})` }}>
-      <h1 className="text-2xl text-white">Search for books</h1>
-      <div className="flex flex-col gap-3 sm:flex-row sm:w-1/2 mx-auto items-center space-x-2">
-        <Input type="text" placeholder="What are you looking for?" />
-        <Button type="submit">Search</Button>
-      </div>
-      <div className="flex flex-col gap-5 sm:flex-row justify-center">
-        <div className="text-sm flex items-center gap-3">
-          <div className=" text-white">Categories</div>
-          <Select defaultValue="all">
-            <SelectTrigger className="sm:w-[180px]">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="all">all</SelectItem>
-              <SelectItem value="art">art</SelectItem>
-              <SelectItem value="biography">biography</SelectItem>
-              <SelectItem value="computers">computers</SelectItem>
-              <SelectItem value="history">history</SelectItem>
-              <SelectItem value="medical">medical</SelectItem>
-              <SelectItem value="poetry">poetry</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className=" text-sm flex  items-center gap-3">
-          <div className=" text-white">Sorting by</div>
-          <Select defaultValue="relevance">
-            <SelectTrigger className="sm:w-[180px]">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="relevance">relevance</SelectItem>
-              <SelectItem value="newest">newest</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
+    <div className="">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSumbit)}
+          className="space-y-5 p-7 bg-cover bg-center"
+          style={{ backgroundImage: `url(${imgUrl})` }}
+        >
+          <h1 className="text-2xl text-white">Search for books</h1>
+          <div className="flex flex-col gap-3 sm:flex-row w-1/2 mx-auto items-center space-x-2">
+            <FormField
+              control={form.control}
+              name="query"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="What are you looking for?"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+          <div className="flex flex-col gap-5 sm:flex-row justify-center">
+            <div className="text-sm flex items-center gap-3">
+              <div className=" text-white">Categories</div>
+              <FormField
+                control={form.control}
+                name="category"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormControl>
+                      <Select
+                        defaultValue={field.value}
+                        onValueChange={field.onChange}
+                      >
+                        <SelectTrigger className="sm:w-[180px]">
+                          <SelectValue />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="all">all</SelectItem>
+                          <SelectItem value="art">art</SelectItem>
+                          <SelectItem value="biography">biography</SelectItem>
+                          <SelectItem value="computers">computers</SelectItem>
+                          <SelectItem value="history">history</SelectItem>
+                          <SelectItem value="medical">medical</SelectItem>
+                          <SelectItem value="poetry">poetry</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className=" text-sm flex  items-center gap-3">
+              <div className=" text-white">Sorting by</div>
+              <FormField
+                control={form.control}
+                name="sort"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="sm:w-[180px]">
+                          <SelectValue />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="relevance">relevance</SelectItem>
+                        <SelectItem value="newest">newest</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
