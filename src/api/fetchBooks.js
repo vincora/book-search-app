2e6d868d68e5c5191ee6e155cd3ba2a5ba2a79d3
Yulia Glagola <julichis@mail.ru>
2024-02-03 23:59:25 +0300

    added filter by category and sort in api fetch

--- src/api/fetchBooks.js
+++ src/api/fetchBooks.js
@@ -1,13 +1,15 @@
 const apiKey = encodeURI(import.meta.env.VITE_GOOGLE_BOOKS_API_KEY);
 
-export const fetchBooks = async (query) => {
-  const searchQuery = encodeURI(query.input);
-  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&key=${apiKey}`;
-
+export const fetchBooks = async ({input, category, sort}) => {
+  const query = [ input ];
+  if( category !== 'all'){
+    query.push(`subject:${category}`);
+  }
+  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query.join('+'))}&orderBy=${sort}&maxResults=30&key=${apiKey}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
 
-  return {
+  return  {
     total: data.totalItems,
     items: data.items.map((item) => ({
       title: item.volumeInfo.title,
